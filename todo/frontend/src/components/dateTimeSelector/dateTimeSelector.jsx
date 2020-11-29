0074bbde4c694c230b9a2017d229d624613903ad
daniriian <daniriian@gmail.com>
2020-11-29 20:07:09 +0200

    added next and prev buttons to modal

--- todo/frontend/src/components/dateTimeSelector/dateTimeSelector.jsx
+++ todo/frontend/src/components/dateTimeSelector/dateTimeSelector.jsx
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
-import DatePicker from "react-datepicker";
+import DatePicker from 'react-datepicker';
+import TimePicker from '../timePicker/timePicker';
 
-import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker.css';
 
-const DateTimeSelector = () => {
+const DateTimeSelector = (props) => {
   const [startDate, setStartDate] = useState(new Date());
+
+  const handleSetStartDate = (selectedDate) => {
+    props.retrieveDate(selectedDate);
+    setStartDate(selectedDate);
+  };
+
   return (
     <div>
-      <DatePicker selected={startDate} onChange={date => setStartDate(date)} dateFormat="dd.MM.yyyy" />
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => handleSetStartDate(date)}
+        dateFormat="dd.MM.yyyy"
+      />
       {/* <input type="time"></input> */}
+      <TimePicker />
     </div>
   );
 };
